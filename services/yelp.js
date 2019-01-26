6b51ce663f7d1aa8cdab20694946bb27e688a23d
josephttran <42635012+josephttran@users.noreply.github.com>
2019-01-26 02:21:24 -0800

    Search businesses with latitude, longitude, term

--- services/yelp.js
+++ services/yelp.js
@@ -1,25 +1,15 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const baseUrl = process.env.YELP_URL;
-
 class Yelp {
     static async searchBusinessResult(latitude, longitude, term) {
-        const urlResult = `${baseUrl}/businesses/search`
+        const urlResult = `${process.env.YELP_URL}/businesses/search?latitude=${latitude}&longitude=${longitude}&term=${term}`
         const config = {
             headers: {
                 Authorization: `bearer ${process.env.YELP_API_KEY}`
             },
-            // auth: {
-            //     "bearer": process.env.YELP_API_KEY
-            // },
-            latitude: latitude,
-            longitude: longitude,
-            term: term
         }
-
         const res = await axios.get(urlResult, config)
-        console.log(res.data);
         return res.data;
     }
 }
