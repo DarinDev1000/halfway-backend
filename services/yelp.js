8f3647f2856177a1310267c310e0695fee3fda62
Darin <darindev1000@gmail.com>
2019-01-26 04:45:10 -0800

    merge

--- services/yelp.js
+++ services/yelp.js
@@ -13,20 +13,10 @@ class Yelp {
         return res.data;
     }
 
-<<<<<<< HEAD
     static async searchBusinessResultPost(latitude, longitude, categories) {
         const catStr = categories.join(',');
 
         const urlResult = `${process.env.YELP_URL}/businesses/search?latitude=${latitude}&longitude=${longitude}&categories=${catStr}`;
-=======
-    static async searchBusinessResultPost(latitude, longitude, term) {
-        let terms = "";
-        for (const t of term) {
-            terms = `${terms}&term=${t}`;
-        }
-        console.log(terms);
-        const urlResult = `${process.env.YELP_URL}/businesses/search?latitude=${latitude}&longitude=${longitude}${terms}`;
->>>>>>> 543143dc61faca8a0d354c608348a8d093c135e1
         console.log(urlResult);
         const config = {
             headers: {
