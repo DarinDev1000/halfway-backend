be729298eae490c345c5cec100083ccf3b788ab7
josephttran <42635012+josephttran@users.noreply.github.com>
2019-01-26 01:19:05 -0800

    yelp api start

---
+++ controllers/yelpApi.js
@@ -0,0 +1,19 @@
+const yelpService = require('../models/services/yelp');
+
+class YelpApi {
+    static async getBusinessResult(ctx) {
+        try {
+            ctx.body = await yelpService.searchBusinessResult(
+                ctx.params.lat, 
+                ctx.params.long, 
+                ctx.params.term
+            )            
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
+
+    }
+}
+
+module.exports = YelpApi;
--- routes/routes-root.js
+++ routes/routes-root.js
@@ -6,11 +6,11 @@
 
 const router = require('koa-router')(); // router middleware for koa
 const sample = require('../models/sample');
-
+const yelp = require('../controllers/yelpApi');
 
 router.get('/', sample.sampleFunction);
 router.get('/json', sample.sampleJson);
-
+router.get('/yelpData/:lat/:long/:term', yelp.getBusinessResult);
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
 module.exports = router.middleware();
